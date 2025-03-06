82284d6d78332c03f32e5733dddb1730c2357d6f
“[ajaykumar <[nabalsaini231@gmail.com]>
2025-03-07 02:03:17 +0530

    deploy

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const cors = require("cors");
+app.use(cors());  
+
+
 // app.get("/listSchools", (req, res) => {
 //     db.query("SELECT * FROM schools", (err, results) => {
 //         if (err) {
