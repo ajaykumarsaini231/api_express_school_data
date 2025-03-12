37a075372f5d2411ed0cf9ac525a139263d142cd
“[ajaykumar <[nabalsaini231@gmail.com]>
2025-03-13 02:50:01 +0530

    hello sir

--- connection.js
+++ connection.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2");
-
+const dotenv = require('dotenv')
 // Create a connection with the provided database name
+dotenv.config();
+
 const db = mysql.createConnection({
-  host: "sql7.freesqldatabase.com",
-  user: "sql7766382",
-  password: "Cl14PvrwLI",
-  database: "sql7766382"  
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE 
 });
 
 // Connect to MySQL
